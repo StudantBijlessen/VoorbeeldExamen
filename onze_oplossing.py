94efa6b2cd82ceeede3dfa04e3f23dc2800131fa
Rob Mandelings <rob@minetronic.com>
2022-05-20 21:08:11 +0200

    Converted to using set

--- onze_oplossing.py
+++ onze_oplossing.py
@@ -1,7 +1,6 @@
 def main():
     try:
         records = leesInput('test3-voorbeeld2.txt')
-        records = leesInput('test3-voorbeeld2.txt')
 
         kring = input('Van welke kring wil je de leden afdrukken?')
         print(f'Je hebt gekozen voor {kring}!')
@@ -13,71 +12,43 @@ def main():
         print('Zorg voor de juiste input!')
 
 
-def bestaat(records, record):
-    record_bestaat = False
-    for current_record in records:
-        for i in range(len(records)):
-            if current_record == record:
-                record_bestaat = True
-
-    return record_bestaat
-
-
 def leesInput(filename):
     with open(filename, 'r') as input_file:
         veldnamen = input_file.readline().rstrip('\n')
         veldnamen = veldnamen.split(';')
 
-        records = list()
+        records = set()
 
         lineNr = 2
         for line in input_file.readlines():
             line = line.rstrip('\n')
 
-            record_elements = line.split(';')
+            record_elements = tuple(line.split(';'))
 
             if len(record_elements) != len(veldnamen):
                 raise Exception(
                     f'Lijn {lineNr}: aantal veldnamen moet {len(veldnamen)} zijn, maar is {len(record_elements)}')
 
-            new_record = \
-                {
-                    veldnamen[0]: record_elements[0],
-                    veldnamen[1]: record_elements[1],
-                    veldnamen[2]: record_elements[2]
-                }
-
-            record_bestaat = bestaat(records, new_record)
-
-            if not record_bestaat:
-                records.append(new_record)
-            else:
-                print('Record bestaat!')
+            records.add(record_elements)
 
             lineNr += 1
 
         return records
 
 
-def print_kring_leden(records: list, kring: str):
-    """
-    Schrijf de gemaakte keuze voor een opgevraagde kring en de namen van de kringleden met de bijhorende code,naar een bestand resultaat-voorbeeld.txt.
-    :param records:
-    :return:
-    """
-
+def print_kring_leden(records, kring):
     with open('resultaat-voorbeeld.txt', 'w+') as out_file:
         out_file.write(f'Kring: {kring}\n')
         for record in records:
-            if record['Kring'] == kring:
-                out_file.write(f'{record["Naam"]} - {record["Code"]}\n')
+            if record[2] == kring:
+                out_file.write(f'{record[0]} - {record[1]}\n')
 
 
 def aantal_leden_per_kring(records):
     leden = dict()
 
     for record in records:
-        kring = record['Kring']
+        kring = record[2]
         if kring in leden:
             leden[kring] += 1
         else:
--- test3-voorbeeld-opl.py
+++ test3-voorbeeld-opl.py
@@ -8,49 +8,58 @@
 # Bepaal en toon per kring het aantal leden.
 
 def main():
-    gegevens=inlezen('test3-voorbeeld.txt')
-    resultaat=maaklijst(gegevens)
-    #print(resultaat)
-    namen_en_code,gewenste_kring=bepaalnamen(resultaat)
-    schrijven(gewenste_kring,namen_en_code,'resultaat-voorbeeld.txt')
+    gegevens = inlezen('test3-voorbeeld.txt')
+    resultaat = maaklijst(gegevens)
+    # print(resultaat)
+    namen_en_code, gewenste_kring = bepaalnamen(resultaat)
+    schrijven(gewenste_kring, namen_en_code, 'resultaat-voorbeeld.txt')
+
+
 def inlezen(bestand):
-    openbestandl=open(bestand,'r')
-    inhoud=openbestandl.readlines()
+    openbestandl = open(bestand, 'r')
+    inhoud = openbestandl.readlines()
     openbestandl.close()
     return inhoud
+
+
 def maaklijst(bronlijst):
     del bronlijst[0]
     # elke record met naam, code en kring in bronlijst
     for i in range(len(bronlijst)):
-        bronlijst[i] = bronlijst[i].rstrip()# alle whitespaces
+        bronlijst[i] = bronlijst[i].rstrip()  # alle whitespaces
     # 3 items per record
-    setlijst=set(bronlijst) #unieke records
-    nieuwelijst=[]
+    setlijst = set(bronlijst)  # unieke records
+    nieuwelijst = []
     for elke_record in setlijst:
         nieuwelijst.append(elke_record.split(';'))
     return nieuwelijst
+
+
 def bepaalnamen(lijst):
     # Welke gegevens hebben we nodig?
-    naam_code=[]
-    kring={}
-    gezochte_kring=input('Van welke kring wil je de namen met code? ')
+    naam_code = []
+    kring = {}
+    gezochte_kring = input('Van welke kring wil je de namen met code? ')
     for i in range(len(lijst)):
-        if lijst[i][2]==gezochte_kring:
+        if lijst[i][2] == gezochte_kring:
             naam_code.append(lijst[i][0] + ' ' + lijst[i][1])
         if lijst[i][2] in kring:
-            kring[lijst[i][2]]+=1
+            kring[lijst[i][2]] += 1
         else:
-            kring[lijst[i][2]]=1
+            kring[lijst[i][2]] = 1
     # alle kringen tonen met hun aantal
     for een_kring in kring:
-        print(een_kring,kring[een_kring])
+        print(een_kring, kring[een_kring])
     return naam_code, gezochte_kring
-def schrijven(gezochte_kring,namenlijst,bestand):
+
+
+def schrijven(gezochte_kring, namenlijst, bestand):
     namenlijst.sort()
-    openbestands=open(bestand,'w')
-    openbestands.write('Kring: ' + gezochte_kring +'\n')
+    openbestands = open(bestand, 'w')
+    openbestands.write('Kring: ' + gezochte_kring + '\n')
     for ind in range(len(namenlijst)):
-        openbestands.write(namenlijst[ind]+'\n')
+        openbestands.write(namenlijst[ind] + '\n')
     openbestands.close()
+
 
 main()
